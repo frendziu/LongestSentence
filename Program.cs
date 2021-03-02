d79db2d57d0ab538d84a93d951efb23a62b46d70
Kamil Ferenc <kamilf20@gmail.com>
2021-03-02 23:41:10 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LongestSentence
+{
+    class Program
+    {
+        static int solution(string S)
+        {
+            int count = 0;
+            int maxCount = 0;
+            string[] sentences = S.Split('.', '?', '!');
+            
+
+            foreach (string sentence in sentences)
+            {
+                string[] words = sentence.Split(' ');
+                
+                foreach(string word in words)
+                {
+                    if(!String.IsNullOrEmpty(word))
+                    count++;
+                }
+                if(maxCount<count)
+                maxCount = count;
+                count = 0;
+            }
+
+            return maxCount;
+        }
+
+        static void Main(string[] args)
+        {
+            string sentence1 = "We test coders. Give us a try?";
+            string sentence2 = "Forget CVs..Save time . x x";
+
+            Console.WriteLine("The largest number of words in: " + sentence1 + " is: " + solution(sentence1));
+            Console.WriteLine("The largest number of words in: " + sentence2 + " is: " + solution(sentence2));
+        }
+    }
+}
